ba5ce254300d06c2192a9eefd772bcee55fdc64b
David De Anda <omarandstuff@gmail.com>
2018-06-15 20:51:19 -0500

    Printer class to print some nice lines

---
+++ src/Printer.js
@@ -0,0 +1,136 @@
+export default class Printer {
+  drawRow(elements, inline = false) {
+    const terminalWidth = process.stdout.columns
+    const processedElements = this._fitElements(elements, terminalWidth)
+    const fixedWidth = this._calculateFixedElementsWidth(processedElements)
+    const availableWidth = Math.max(0, terminalWidth - fixedWidth)
+    const rendered = this._buildAndFormat(processedElements, availableWidth)
+
+    if (inline) {
+      process.stdout.write(`${rendered}\r`)
+    } else {
+      process.stdout.write(`${' '.repeat(terminalWidth)}\r`)
+      console.log(rendered)
+    }
+  }
+
+  draw(elements) {
+    const terminalWidth = process.stdout.columns
+
+    process.stdout.write(`${' '.repeat(terminalWidth)}\r`)
+    console.log(this._buildAndFormat(elements))
+  }
+
+  _applyFormat(element, optionalText) {
+    if (element.style) {
+      return element.style(optionalText || element.text)
+    }
+    return optionalText || element.text
+  }
+
+  _buildAndFormat(elements, availableWidth) {
+    const dynamicCount = this._calculateDynamicCount(elements)
+    const widthPerDynamicElement = Number.parseInt(availableWidth / dynamicCount)
+    const specialDynamic = widthPerDynamicElement === 0
+    let availableDynamixSpace = availableWidth
+    let uncalculateDynamicSpace = availableWidth - widthPerDynamicElement * dynamicCount
+
+    return elements
+      .map(element => {
+        if (element.blank) {
+          if (specialDynamic) {
+            if (availableDynamixSpace) {
+              availableDynamixSpace--
+              return this._applyFormat(element, ' ')
+            }
+          } else {
+            const extraSpace = uncalculateDynamicSpace-- > 0 ? 1 : 0
+            const blank = ' '.repeat(widthPerDynamicElement + extraSpace)
+            return this._applyFormat(element, blank)
+          }
+        } else if (element.fit) {
+          if (specialDynamic) {
+            if (availableDynamixSpace) {
+              availableDynamixSpace--
+              return this._applyFormat(element, element.text[0])
+            }
+          } else {
+            const extraSpace = uncalculateDynamicSpace-- > 0 ? 1 : 0
+            const finalWith = widthPerDynamicElement + extraSpace
+
+            if (element.text.length > finalWith) {
+              const exceed = element.text.length - finalWith
+              const cutPosition = element.text.length - exceed
+              const addDots = cutPosition - 3 > 0
+              const extraCut = addDots ? 3 : 0
+              const extraDots = addDots ? '...' : ''
+              const cuttedText = element.text.substring(0, cutPosition - extraCut)
+              const rendered = this._applyFormat(element, cuttedText)
+
+              return `${rendered}${extraDots}`
+            } else {
+              const lack = finalWith - element.text.length
+              const amplifiedText = `${element.text}${' '.repeat(lack)}`
+              const rendered = this._applyFormat(element, amplifiedText)
+
+              return `${rendered}`
+            }
+          }
+        } else {
+          return this._applyFormat(element)
+        }
+      })
+      .join('')
+  }
+
+  _calculateDynamicCount(elements, onlyBlanks) {
+    return elements.reduce((currentCount, element) => {
+      if ((element.fit && !onlyBlanks) || element.blank) {
+        return currentCount + 1
+      }
+      return currentCount
+    }, 0)
+  }
+
+  _calculateFixedElementsWidth(elements) {
+    return elements.reduce((currentWidth, element) => {
+      if (!element.fit && !element.blank) {
+        return currentWidth + element.text.length
+      }
+      return currentWidth
+    }, 0)
+  }
+
+  _fitElements(elements, targetWidth) {
+    const fixedWidth = this._calculateFixedElementsWidth(elements)
+    const availableWidth = Math.max(0, targetWidth - fixedWidth)
+    const dynamicCount = this._calculateDynamicCount(elements)
+    const widthPerDynamicElement = Number.parseInt(availableWidth / dynamicCount)
+    const removeDynamics = fixedWidth >= targetWidth
+    let currentWidth = 0
+
+    return elements
+      .map(element => {
+        if (currentWidth < targetWidth) {
+          if (element.fit || element.blank) {
+            if (!removeDynamics) {
+              currentWidth += widthPerDynamicElement
+              return element
+            }
+          } else {
+            currentWidth += element.text.length
+
+            if (currentWidth >= targetWidth) {
+              const exceed = currentWidth - targetWidth
+              const cutPosition = element.text.length - exceed
+
+              return { ...element, text: element.text.substring(0, cutPosition) }
+            }
+
+            return element
+          }
+        }
+      })
+      .filter(element => element)
+  }
+}
---
+++ tests/Printer.test.js
@@ -0,0 +1,181 @@
+import Printer from '../src/Printer'
+
+const realLog = console.log
+const realWrite = process.stdout.write
+const realColumns = process.stdout.columns
+
+describe('Printer#drawRow', () => {
+  beforeEach(() => {
+    console.log = jest.fn()
+    process.stdout.write = jest.fn()
+    process.stdout.columns = 10
+  })
+
+  afterAll(() => {
+    console.log = realLog
+    process.stdout.write = realWrite
+    process.stdout.columns = realColumns
+  })
+
+  describe('Printing inline', () => {
+    it('it adds a /r at the end and uses process.stdout.write', async () => {
+      const printer = new Printer()
+
+      printer.drawRow([{ text: 'some text' }], true)
+
+      expect(process.stdout.write.mock.calls[0][0]).toBe('some text\r')
+    })
+  })
+
+  describe('Printing short text smaller than the terminal', () => {
+    it('just prints that shor text', async () => {
+      const printer = new Printer()
+
+      printer.drawRow([{ text: 'short' }])
+
+      expect(console.log.mock.calls[0][0]).toBe('short')
+    })
+  })
+
+  describe('trying to print more text than space available', () => {
+    it('crops the text', async () => {
+      const printer = new Printer()
+
+      printer.drawRow([{ text: 'this is a very large text' }])
+
+      expect(console.log.mock.calls[0][0]).toBe('this is a ')
+    })
+
+    describe('throuh multiple text elements', () => {
+      it('still crops the text', async () => {
+        const printer = new Printer()
+
+        printer.drawRow([{ text: 'this ' }, { text: 'is ' }, { text: 'a ' }, { text: 'very ' }, { text: 'large text' }])
+
+        expect(console.log.mock.calls[0][0]).toBe('this is a ')
+      })
+    })
+  })
+
+  describe('Including text elements set as dynamic (fit: true)', () => {
+    it('fits the text and add ...', async () => {
+      const printer = new Printer()
+
+      printer.drawRow([{ text: 'dynamic text', fit: true }])
+
+      expect(console.log.mock.calls[0][0]).toBe('dynamic...')
+    })
+
+    describe('throuh multiple text elements', () => {
+      it('still fits the text', async () => {
+        const printer = new Printer()
+
+        printer.drawRow([{ text: 'short ' }, { text: 'dynamic text', fit: true }])
+
+        expect(console.log.mock.calls[0][0]).toBe('short d...')
+      })
+    })
+
+    describe('the dynamic text is too short for the available space', () => {
+      it('adds extra spaces to fill space', async () => {
+        const printer = new Printer()
+
+        printer.drawRow([{ text: '22', fit: true }])
+
+        expect(console.log.mock.calls[0][0]).toBe('22        ')
+      })
+    })
+
+    describe('when there is not space for the 3 dots', () => {
+      it('dont use the dots', async () => {
+        const printer = new Printer()
+
+        printer.drawRow([{ text: 'bigenough' }, { text: 'dynamic text', fit: true }])
+
+        expect(console.log.mock.calls[0][0]).toBe('bigenoughd')
+      })
+    })
+
+    describe('when it is not posible to assign even space for all dynamics and blanks', () => {
+      it('add some more spaces for the first ones', async () => {
+        const printer = new Printer()
+
+        process.stdout.columns = 11
+        printer.drawRow([{ text: 'dynamic text', fit: true }, { blank: true }, { text: 'dynamic text', fit: true }])
+
+        expect(console.log.mock.calls[0][0]).toBe('d...    dyn')
+      })
+    })
+
+    describe('when there are too many dynamics for available space', () => {
+      it('just renders some of them', async () => {
+        const printer = new Printer()
+
+        process.stdout.columns = 5
+        printer.drawRow([
+          { text: 'dynamic text', fit: true },
+          { blank: true },
+          { text: 'dynamic text', fit: true },
+          { blank: true },
+          { text: 'dynamic text', fit: true },
+          { text: 'dynamic text', fit: true },
+          { blank: true }
+        ])
+
+        expect(console.log.mock.calls[0][0]).toBe('d d d')
+      })
+    })
+  })
+
+  describe('when static text exeed available space', () => {
+    it('do not try to render any dinamic text element', async () => {
+      const printer = new Printer()
+
+      printer.drawRow([{ text: 'short ' }, { text: 'dynamic text', fit: true }, { text: 'not too short ' }])
+
+      expect(console.log.mock.calls[0][0]).toBe('short not ')
+    })
+  })
+
+  describe('efining blank block to fill space', () => {
+    it('fills the space between text', async () => {
+      const printer = new Printer()
+
+      printer.drawRow([{ text: 'short' }, { blank: true }, { text: '22' }])
+
+      expect(console.log.mock.calls[0][0]).toBe('short   22')
+    })
+  })
+})
+
+describe('Printer#drawRow', () => {
+  beforeEach(() => {
+    console.log = jest.fn()
+    process.stdout.write = jest.fn()
+    process.stdout.columns = 10
+  })
+
+  afterAll(() => {
+    console.log = realLog
+    process.stdout.write = realWrite
+    process.stdout.columns = realColumns
+  })
+
+  it('justs print the text given and apply formats', () => {
+    const printer = new Printer()
+    const styleCall = jest.fn()
+
+    printer.draw([
+      {
+        text: 'Some random text',
+        style: string => {
+          styleCall()
+          return string
+        }
+      }
+    ])
+
+    expect(console.log.mock.calls[0][0]).toBe('Some random text')
+    expect(styleCall.mock.calls.length).toBe(1)
+  })
+})
