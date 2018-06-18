f2f722d631c9537ea2148984573a3080dc974b06
David De Anda <omarandstuff@gmail.com>
2018-06-17 21:54:29 -0500

    ssh mock now can simulate multiple errors and connection interruptions

--- tests/__mocks__/ssh2.js
+++ tests/__mocks__/ssh2.js
@@ -2,9 +2,11 @@ import EventEmitter from 'events'
 import Stream from './stream'
 
 export class Client extends EventEmitter {
-  static __mockConnectionError = false
-  static __mockExecError = false
-  static __mockExecErrorCode = false
+  static __mockConnectionError = 0
+  static __mockExecError = 0
+  static __mockExecErrorCode = 0
+  static __mockExecTimeOut = 0
+  static __mockConnectionInterruption = 0
 
   constructor() {
     super()
@@ -16,7 +18,7 @@ export class Client extends EventEmitter {
     if (!Client.__mockConnectionError) {
       this.emit('ready')
     } else {
-      Client.__mockConnectionError = false
+      Client.__mockConnectionError--
       this.emit('error', { error: 'Connection error' })
     }
   }
@@ -27,17 +29,58 @@ export class Client extends EventEmitter {
   }
 
   exec(command, callback) {
-    const stream = new Stream(true, Client.__mockExecErrorCode)
-    Client.__mockExecErrorCode = false
-    if (!Client.__mockExecError) {
-      callback(undefined, stream)
-    } else {
-      Client.__mockExecError = false
-      callback({ error: 'Exec error' }, stream)
+    const withErrorCode = Client.__mockExecErrorCode !== 0
+    const withExecError = Client.__mockExecError !== 0
+    const withTimeOut = Client.__mockExecTimeOut !== 0
+    const withInterruption =
+      Client.__mockConnectionInterruption !== 0 &&
+      Client.__mockExecErrorCode + Client.__mockExecError + Client.__mockExecTimeOut < 2
+
+    if (Client.__mockConnectionInterruption !== 0) {
+      if (!Client.__saveForInteruption) {
+        Client.__interruptionExecErrorCodes = Client.__mockExecErrorCode
+        Client.__interruptionExecErrors = Client.__mockExecError
+        Client.__interruptionExecTimeOuts = Client.__mockExecTimeOut
+        Client.__saveForInteruption = true
+      }
     }
 
-    stream.__data()
-    stream.stderr.__data()
-    stream.__close()
+    if (withInterruption) {
+      Client.__mockExecErrorCode = Client.__interruptionExecErrorCodes
+      Client.__mockExecError = Client.__interruptionExecErrors
+      Client.__mockExecTimeOut = Client.__interruptionExecTimeOuts
+      Client.__mockConnectionInterruption = Number(Client.__mockConnectionInterruption) - 1
+
+      if (Client.__mockConnectionInterruption === 0) {
+        Client.__saveForInteruption = false
+      }
+
+      this.emit('error', { error: 'Comunication interrupted' })
+      this.emit('end')
+      this.emit('close')
+
+      callback({ error: 'Comunication interrupted' })
+    } else {
+      const stream = new Stream(true, withErrorCode)
+
+      if (withErrorCode) {
+        Client.__mockExecErrorCode = Number(Client.__mockExecErrorCode) - 1
+      }
+
+      if (!withErrorCode && withExecError) {
+        Client.__mockExecError = Number(Client.__mockExecError) - 1
+        callback({ error: 'Exec error' }, stream)
+      } else {
+        callback(undefined, stream)
+      }
+
+      stream.__data()
+      stream.stderr.__data()
+      if (!withErrorCode && !withExecError && withTimeOut) {
+        Client.__mockExecTimeOut = Number(Client.__mockExecTimeOut) - 1
+      } else {
+        stream.__close()
+      }
+    }
   }
 }
